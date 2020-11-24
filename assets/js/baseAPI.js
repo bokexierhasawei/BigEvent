48b8e641a1d700fc46285fa804d30cca54d60a89
bokexier <bokexier@bokexier.cn>
2020-11-24 17:34:27 +0800

    提交index.html

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,24 @@
 // 注意：每次调用 $.get() 或 $.post() 或 $.ajax() 的时候，
 // 会先调用 ajaxPrefilter 这个函数
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
-$.ajaxPrefilter(function(options) {
+$.ajaxPrefilter(function (options) {
   // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
   options.url = 'http://ajax.frontend.itheima.net' + options.url
+  // 设置header请求头
+  if (options.url.indexOf('/my/') > -1) {
+    options.hearders = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+  // 为所有的ajax请求 统一 配置 complete
+  options.complete = function (res) {
+    console.log(res.responseJSON);
+    console.log(11);
+    if (res.responseJSON.status == 1 && res.responseJSON.message == "身份认证失败！") {
+      console.log(1111);
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+$(function () {
+    getUserInfo();
+    $('#btnLoginout').on('click', logout)
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token')
+        // },
+        success(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败!')
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+function renderAvatar(user) {
+    let name = user.nickname || user.username
+    $('#welcome').html(name)
+    if (user.user_pic !== null) {
+        // 有图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        // 隐藏文字头像
+        $('.text-avatar').hide();
+    } else {
+        // 没有图片头像
+        $('.layui-nav-img').hide()
+        let firstChar = name[0].toUpperCase();
+        $('.text-avatar').text(firstChar).show();
+    }
+}
+function logout() {
+    layui.layer.confirm('really want to leave me?', { icon: 3, title: '提示' }, function (index) {
+        //do something
+        // 删除 localStorage 中的token
+        localStorage.removeItem('token');
+        location.href = "/login.html"
+
+        layer.close(index);
+    });
+}
